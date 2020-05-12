2e227bda38049cc71ad30dd3704c33d29dbeeb62
Drew Peters <peters.drew.dp@gmail.com>
2020-05-12 14:01:33 -0400

    updated models for db/repo communication

--- src/models/reimbursement.ts
+++ src/models/reimbursement.ts
@@ -1,38 +1,38 @@
+import { Receipt } from './receipt';
+
 export class Reimbursement {
 
-    reimb_id: number;
+    id: number;
     amount: number;
-    submitted: string;
-    resolved: string;
+    submitted: Date;
+    resolved: Date;
     description: string;
-    reciept: any;
-    author_id: number;
-    resolver_id: number;
-    reimb_status: string;
-    reimb_type: string;
+    receipt: number; //placeholder
+    author: string;
+    resolver: string;
+    reimb_status_id: number;
+    reimb_type_id: number;
 
-    constructor(
-        id: number,
-        am: number,
-        sub: string,
-        res: string,
-        des: string,
-        rec: any,
-        auth_id: number,
-        res_id: number,
-        rei_sta: string,
-        rei_typ: string
-    ) {
-        this.reimb_id = id;
-        this.amount = am;
-        this.submitted = sub;
-        this.resolved = res;
-        this.description = des;
-        this.reciept = rec;
-        this.author_id = auth_id;
-        this.resolver_id = res_id;
-        this.reimb_status = rei_sta;
-        this.reimb_type = rei_typ;
+    constructor(id: number, 
+                amount: number, 
+                submitted: Date, 
+                resolved: Date, 
+                description: string, 
+                receipt: number, 
+                author: string, 
+                resolver: string, 
+                reimb_status_id: number, 
+                reimb_type_id: number) 
+    {
+        this.id = id;
+        this.amount = amount;
+        this.submitted = submitted;
+        this.resolved = resolved;
+        this.description = description;
+        this.receipt = receipt;
+        this.author = author;
+        this.resolver = resolver;
+        this.reimb_status_id = reimb_status_id;
+        this.reimb_type_id = reimb_type_id;
     }
-
 }
