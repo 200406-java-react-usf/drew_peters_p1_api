58aedcdf470a60ac5c5863cd191208ef3bbfd3e9
Drew Peters <peters.drew.dp@gmail.com>
2020-05-12 14:03:32 -0400

    updated routers with verbs for http comm

--- src/router/reimbursement-router.ts
+++ src/router/reimbursement-router.ts
@@ -1,53 +1,65 @@
 // import url from 'url';
-// import express from 'express';
-// import AppConfig from '../config/app';
+import express from 'express';
+import AppConfig from '../config/app';
 // import { isEmptyObject } from '../util/validator';
 // import { ParsedUrlQuery } from 'querystring';
-// import { adminGuard } from '../middleware/auth-middleware';
+import { adminGuard } from '../middleware/auth-middleware';
 
-// export const ReimbursementRouter = express.Router();
+export const ReimbursementRouter = express.Router();
 
-// const reimbursementService = AppConfig.reimbursementService;
+const reimbursementService = AppConfig.reimbursementService;
 
-// ReimbursementRouter.get('', adminGuard, async (req, resp) => {
+ReimbursementRouter.get('', adminGuard, async (req, resp) => {
+    try {
+        let payload = await reimbursementService.getAllReimbursements();
+        return resp.status(200).json(payload);
+    } catch (e) {
+        resp.status(e.statusCode).json(e);
+    }
+});
 
-//     try {
+ReimbursementRouter.get('/:id', adminGuard, async (req, resp) => {
+    const id = +req.params.id;
+    try {     
+        let payload = await reimbursementService.getReimbursementById(id); 
+        return resp.status(200).json(payload);
+    } catch (e) {
+        return resp.status(e.statusCode).json(e).send();
+    }
+});
 
-//         let reqURL = url.parse(req.url, true);
+ReimbursementRouter.post('', adminGuard, async (req, resp) => {
 
-//         if(!isEmptyObject<ParsedUrlQuery>(reqURL.query)) {
-//             let payload = await reimbursementService.getReimbursementByUniqueKey({...reqURL.query});
-//             resp.status(200).json(payload);
-//         } else {
-//             let payload = await reimbursementService.getAllReimbursements();
-//             resp.status(200).json(payload);
-//         }
+    console.log('REIMBURSEMENT POST REQUEST RECEIVED AT /reimbursement');
+    console.log(req.body);
+    try {
+        let newUser = await reimbursementService.addNewReimbursement(req.body);
+        return resp.status(201).json(newUser).send();
+    } catch (e) {
+        return resp.status(e.statusCode).json(e).send();
+    }
+});
 
-//     } catch (e) {
-//         resp.status(e.statusCode).json(e);
-//     }
+ReimbursementRouter.patch('/:id', adminGuard, async (req, resp) => {
+    const id = +req.params.id;
+    console.log('REIMBURSEMENT UPDATE REQUEST RECEIVED AT /reimbursement');
+    console.log(req.body);
+    try {
+        let status = await reimbursementService.updateReimbursement(id, req.body);
+        return resp.status(204).json(status).send();
+    } catch (e) {
+        return resp.status(e.statusCode).json(e).send();
+    }
+});
 
-// });
-
-// ReimbursementRouter.get('/:id', async (req, resp) => {
-//     const id = +req.params.id;
-//     try {
-//         let payload = await reimbursementService.getReimbursementById(id);
-//         return resp.status(200).json(payload);
-//     } catch (e) {
-//         return resp.status(e.statusCode).json(e);
-//     }
-// });
-
-// ReimbursementRouter.post('', async (req, resp) => {
-
-//     console.log('POST REQUEST RECEIVED AT /reimbursements');
-//     console.log(req.body);
-//     try {
-//         let newReimbursement = await reimbursementService.addNewReimbursement(req.body);
-//         return resp.status(201).json(newReimbursement);
-//     } catch (e) {
-//         return resp.status(e.statusCode).json(e);
-//     }
-
-// });
+ReimbursementRouter.delete('/:id', adminGuard, async (req, resp) => {
+    const id = +req.params.id;
+    console.log('REIMBURSEMENT DELETE REQUEST RECEIVED AT /reimbursement');
+    console.log(req.body);
+    try {
+        let status = await reimbursementService.deleteById(id);
+        return resp.status(204).json(status).send();
+    } catch (e) {
+        return resp.status(e.statusCode).json(e).send();
+    }
+});
