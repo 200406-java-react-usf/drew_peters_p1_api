aaa61a50aee49f1e78ff6755e8888b94ebba8b3b
Drew Peters <peters.drew.dp@gmail.com>
2020-05-22 00:49:46 -0400

    trying to fix cors

--- src/middleware/cors-filter.ts
+++ src/middleware/cors-filter.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 
 export function corsFilter(req: Request, resp: Response, next: any) {
 
-    resp.header('Access-Control-Allow-Origin', 'http://p1-ui.s3-website-us-east-1.amazonaws.com/'); 
+    resp.header('Access-Control-Allow-Origin', 'http://p1-ui.s3-website-us-east-1.amazonaws.com'); 
     resp.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
     resp.header('Access-Control-Allow-Credentials', 'true');
     resp.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
